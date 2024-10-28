3df48814c224c3401cc16bfaeab4c5d66e60c543
Kirito <100836829+BeStg1000@users.noreply.github.com>
2024-10-28 13:34:13 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+import os
+import discord
+from discord.ext import commands
+from discord import app_commands
+
+from myserver import server_on
+
+bot = commands.Bots(command_prefix='!', intents=discord.Intents.all())
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+server_on()
+
+bot.run(os.getenv('TOKEN'))
